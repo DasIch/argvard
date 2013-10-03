46feb4fa8d71c1ef782aa3bd1ff93d3b65bae379
Daniel Neuhäuser <dasdasich@gmail.com>
2013-10-03 20:15:08 +0200

    Add docstrings to utils stuff

--- argvard/utils.py
+++ argvard/utils.py
@@ -30,6 +30,10 @@ _python2_identifier_re = re.compile(r'^[a-zA-Z_][a-zA-Z_0-9]*$')
 
 
 def is_python_identifier(possible_identifier):
+    """
+    Returns `True` if the given `possible_identifier` can be used as an
+    identifier in the Python version used by the executing interpreter.
+    """
     if PY2:
         return is_python2_identifier(possible_identifier)
     else:
@@ -37,11 +41,19 @@ def is_python_identifier(possible_identifier):
 
 
 def is_python2_identifier(possible_identifier):
+    """
+    Returns `True` if the given `possible_identifier` can be used as an
+    identifier in Python 2.
+    """
     match = _python2_identifier_re.match(possible_identifier)
     return bool(match) and not iskeyword(possible_identifier)
 
 
 def is_python3_identifier(possible_identifier):
+    """
+    Returns `True` if the given `possible_identifier` can be used as an
+    identifier in Python 3.
+    """
     possible_identifier = unicodedata.normalize('NFKC', possible_identifier)
     return (
         bool(possible_identifier) and
@@ -73,6 +85,10 @@ def _is_in_id_continue(character):
 
 
 def unique(iterable):
+    """
+    Returns an iterator that yields the first occurence of a hashable item in
+    `iterable`.
+    """
     seen = set()
     for obj in iterable:
         if obj not in seen:
